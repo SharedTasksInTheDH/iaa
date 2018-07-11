1533eca9e20816ea7bdcbe33f7c77414640b0d08
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2018-07-11 12:31:40 +0200

    added tokenization

--- src/main/java/de/unistuttgart/ims/creta/sharedtask/CatmaTei2CSV.java
+++ src/main/java/de/unistuttgart/ims/creta/sharedtask/CatmaTei2CSV.java
@@ -4,12 +4,17 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.Comparator;
+import java.util.Map;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 import org.apache.uima.UIMAException;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.cas.TOP;
 import org.apache.uima.jcas.tcas.Annotation;
@@ -18,6 +23,8 @@ import org.eclipse.collections.api.multimap.MutableMultimap;
 import org.eclipse.collections.impl.factory.Maps;
 import org.eclipse.collections.impl.factory.Multimaps;
 
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
 import de.unistuttgart.ims.creta.sharedtask.iaa.type.Seg;
 import de.unistuttgart.ims.uima.io.xml.GenericXmlReader;
 
@@ -65,15 +72,43 @@ public class CatmaTei2CSV {
 		JCas jcas = JCasFactory.createJCas();
 		reader.read(jcas, new FileInputStream(file));
 
+		SimplePipeline.runPipeline(jcas, AnalysisEngineFactory.createEngineDescription(BreakIteratorSegmenter.class,
+				BreakIteratorSegmenter.PARAM_WRITE_SENTENCE, false));
+		int tokenNumber = 0;
+		for (Token token : JCasUtil.select(jcas, Token.class)) {
+			token.setId(String.valueOf(tokenNumber++));
+		}
+
+		Map<Seg, Collection<Token>> tokenIndex = JCasUtil.indexCovered(jcas, Seg.class, Token.class);
 		int counter = 0;
 		try (CSVPrinter p = new CSVPrinter(getAppendable(), CSVFormat.DEFAULT)) {
 			for (String id : annoMap.keySet()) {
 				Seg first = annoMap.get(id).getFirst();
 				Seg last = annoMap.get(id).getLast();
-				p.printRecord("a" + counter++, getAnnotatorId(), fsDeclMap.get(fsMap.get(id)), null, first.getBegin(),
-						last.getEnd());
+				int begin = Integer.parseInt(getFirstToken(tokenIndex.get(first)).getId());
+				int end = Integer.parseInt(getLastToken(tokenIndex.get(last)).getId());
+				p.printRecord(getAnnotatorId().substring(0, 1) + counter++, getAnnotatorId(),
+						fsDeclMap.get(fsMap.get(id)), null, begin, end);
 			}
 		}
+	}
+
+	private Token getFirstToken(Collection<Token> coll) {
+		Token firstToken = null;
+		for (Token token : coll) {
+			if (firstToken == null || token.getBegin() < firstToken.getBegin())
+				firstToken = token;
+		}
+		return firstToken;
+	}
+
+	private Token getLastToken(Collection<Token> coll) {
+		Token lastToken = null;
+		for (Token token : coll) {
+			if (lastToken == null || token.getEnd() > lastToken.getEnd())
+				lastToken = token;
+		}
+		return lastToken;
 	}
 
 	public Appendable getAppendable() {
